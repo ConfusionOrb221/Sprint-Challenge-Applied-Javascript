7941791adebbadf7ad818e1f78525a3c97071ca3
ConfusionOrb221 <nate1898@gmail.com>
2019-11-08 22:19:49 -0500

    ah crap i did it again
    well ive completed everything including the stretch except for animation. So ill explain some of it. For the cards i did some crazy stuff i used the for/in as the response.data.articles isnt enumerable this returns the keys of the articls thehn i use that to do a foreach loop for the articles and set the card data that way. The other ones were really simple so ill explain carousel in this one i create a const array imgs and add all the img elements to it i also set an index of 0. Then when creating the caraousel i set the first image as visible. in the move function i set a variabled called previndex before changing index this is so that i can edit the value of the previous img display property and the current. oh and i check which button triggered the event and do the proper math accordingly.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,40 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContent = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response =>{
+         // this is crazy good code if i dont say so myself
+        for (const keys in response.data.articles){
+            response.data.articles[keys].forEach(content =>{
+                const card = document.createElement('div');
+                const headline = document.createElement('div');
+                const author = document.createElement('div');
+                const imgContainer = document.createElement('div');
+                const img = document.createElement('img');
+                const authorName = document.createElement('span');
+
+                card.className = 'card';
+
+                headline.className = 'headline';
+                headline.textContent = content.headline;
+
+                author.className = 'author';
+
+                imgContainer.className = 'img-container';
+                img.src = content.authorPhoto;
+
+                authorName.textContent = `By ${content.authorName}`;
+
+                card.append(headline);
+                imgContainer.append(img);
+                author.append(imgContainer);
+                author.append(authorName);
+                card.append(author);
+
+                cardsContent.append(card);
+            });
+        }
+    })
+    .catch(err => console.log(err));
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,68 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carouselContainer = document.querySelector('.carousel-container');
+const imgs = [];
+let index = 0;
+
+function createCarousel(){
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('leftButton');
+  const rightButton = document.createElement('div');
+  const img1 = document.createElement('img');
+  const img2 = document.createElement('img');
+  const img3 = document.createElement('img');
+  const img4 = document.createElement('img');
+
+  carousel.className = 'carousel';
+  
+  leftButton.className = 'left-button';
+  leftButton.textContent = '<';
+  leftButton.addEventListener('click', move, false);
+  rightButton.className = 'right-button';
+  rightButton.textContent = '>';
+  rightButton.addEventListener('click', move, false);
+
+  img1.src = './assets/carousel/mountains.jpeg';
+  img1.style.display = 'inline-block';
+  img2.src = './assets/carousel/computer.jpeg';
+  img3.src = './assets/carousel/trees.jpeg';
+  img4.src = './assets/carousel/turntable.jpeg';
+
+  imgs.push(img1);
+  imgs.push(img2);
+  imgs.push(img3);
+  imgs.push(img4);
+
+  carousel.append(leftButton);
+  carousel.append(img1);
+  carousel.append(img2);
+  carousel.append(img3);
+  carousel.append(img4);
+  carousel.append(rightButton);
+
+  carouselContainer.append(carousel);
+} createCarousel();
+
+function move(event){
+  let indexPrev = index;
+  if(event.target.className === 'left-button'){
+    if(index === 0){
+      index = 3;
+    }
+    else{
+      index -= 1;
+    }
+  }
+  else if(event.target.className === 'right-button'){
+    if(index === 3){
+      index = 0;
+    }
+    else{
+      index += 1;
+    }
+  }
+  imgs[indexPrev].style.display = 'none';
+  imgs[index].style.display = 'inline-block';
+}
